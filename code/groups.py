3b44c9378180c9e48bca6f28ab1d0737c3d951af
Abhijeet-1409 <abhijeetjoy14@gmail.com>
2025-05-22 01:07:40 +0530

    Add AllSprites class

---
+++ code/groups.py
@@ -0,0 +1,25 @@
+from settings import *
+from support import *
+from entities import *
+
+
+class AllSprites(pygame.sprite.Group):
+
+    def __init__(self, *sprites):
+        super().__init__(*sprites)
+        self.display_surface = pygame.display.get_surface()
+        self.offset = vector()
+        self.shadow_surf = import_image('graphics','other','shadow')
+        self.notice_surf = import_image('graphics','ui','notice')
+
+    def draw(self,  player: Player):
+        self.offset.x = -(player.rect.centerx - (WINDOW_WIDTH/2))
+        self.offset.y = -(player.rect.centery - (WINDOW_HEIGHT/2))
+
+        bg_sprites = [ sprite for sprite in self.sprites() if sprite.z_index < WORLD_LAYERS['main']]
+        main_sprites = sorted([ sprite for sprite in self.sprites() if sprite.z_index == WORLD_LAYERS['main']], key=lambda sprite: sprite.y_sort)
+        fg_sprites = [ sprite for sprite in self.sprites() if sprite.z_index > WORLD_LAYERS['main']]
+
+        for layer in [bg_sprites, main_sprites, fg_sprites]:
+            for sprite in layer:
+                self.display_surface.blit(sprite.image,sprite.rect.topleft + self.offset)
