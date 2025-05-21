e22650470ecf81c5e4e1ae4854f30c02d248afbc
Abhijeet-1409 <abhijeetjoy14@gmail.com>
2025-05-22 01:15:19 +0530

    Add AnimatedSprite class

--- code/sprites.py
+++ code/sprites.py
@@ -13,3 +13,18 @@ class BaseSprite(pygame.sprite.Sprite):
         self.hitbox = self.rect.copy()
 
 
+class AnimatedSprite(BaseSprite):
+
+    def __init__(self, pos: tuple[float, float], frames: list[Surface], groups: tuple[pygame.sprite.Group], z_index = WORLD_LAYERS['main']):
+        self.frame_index, self.frames = 0, frames
+        super().__init__(pos, frames[self.frame_index], groups, z_index)
+
+    def animate(self, dt: float):
+        self.frame_index += (ANIMATION_SPEED * dt)
+        self.image = self.frames[int(self.frame_index) % len(self.frames)]
+
+    def update(self, dt: float, *args, **kwargs):
+        super().update(*args, **kwargs)
+        self.animate(dt)
+
+
