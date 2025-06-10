012141094834be62c32b5098c95c71239fbe27a6
Abhijeet-1409 <abhijeetjoy14@gmail.com>
2025-06-10 16:55:05 +0530

    Add _repr__, reduce_energy, get_base_damage, update_xp and stat_limiter to Monster class

--- code/monster.py
+++ code/monster.py
@@ -1,4 +1,5 @@
 from random import randint
+from typing import Literal
 
 from settings import *
 from game_data import *
@@ -15,14 +16,17 @@ class Monster():
         self.base_stats: dict[str, str | float] = MONSTER_DATA[name]['stats']
         self.health = self.base_stats['max_health'] * self.level
         self.energy = self.base_stats['max_energy'] * self.level
-        self.health -= randint(0,self.health//2)
-        self.energy -= randint(0,self.energy//2)
         self.initiative = 0
         self.abilities: dict[int, str] = MONSTER_DATA[name]['abilities']
+        self.defending = False
 
         # experience
-        self.xp = randint(0,1000)
+        self.xp = 0
         self.level_up = self.level * 150
+        self.evolution: tuple[str, int] | None = MONSTER_DATA[self.name]['evolve']
+
+    def __repr__(self):
+        return f"Monster: {self.name}, Lvl: {self.level}"
 
     def get_stat(self, stat: str):
         return self.base_stats[stat] * self.level
@@ -40,7 +44,7 @@ class Monster():
     def get_abilities(self, all: bool = True):
         if all:
             return [ ability for level, ability in self.abilities.items() if self.level >= level]
-        return [ ability for level, ability in self.abilities.items() if self.level >= level and self.energy >= ATTACK_DATA[ability]['cost'] ]
+        return [ ability for level, ability in self.abilities.items() if self.level >= level and ATTACK_DATA[ability]['cost'] < self.energy]
 
     def get_info(self):
         return (
@@ -49,6 +53,27 @@ class Monster():
             (self.initiative,100)
         )
 
+    def reduce_energy(self, attack: Literal['burn','heal','battlecry','spark','scratch','splash','fire','explosion','annihilate','ice']):
+        self.energy -= ATTACK_DATA[attack]['cost']
+
+    def get_base_damage(self, attack: Literal['burn','heal','battlecry','spark','scratch','splash','fire','explosion','annihilate','ice']):
+        return self.get_stat('attack') * ATTACK_DATA[attack]['amount']
+
+    def update_xp(self, amount: float):
+        if self.level_up - self.xp > amount:
+            self.xp += amount
+        else:
+            self.xp = amount - (self.level_up - self.xp)
+            self.level += 1
+            self.level_up = self.level * 150
+
+    def stat_limiter(self):
+        self.health = max(0, min(self.health, self.get_stat('max_health')))
+        self.energy = max(0, min(self.energy, self.get_stat('max_energy')))
+
     def update(self, dt: float):
+        self.stat_limiter()
         if not self.paused:
             self.initiative += self.get_stat('speed') * dt
+            self.energy += self.get_stat('recovery') * 0.009 * dt
+            self.stat_limiter()
